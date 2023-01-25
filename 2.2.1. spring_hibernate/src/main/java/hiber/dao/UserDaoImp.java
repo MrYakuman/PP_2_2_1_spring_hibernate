5e3a74af869615b983a456183e883c571912ffab
Pereverzev Ivan <ivan.pereverzev.1997@mail.ru>
2023-01-25 23:21:04 +0400

    1.0

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,11 +1,15 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+//import sun.jvm.hotspot.debugger.Address;
 
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -22,8 +26,19 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
+   }
+
+   @Override
+   @Transactional
+   public User getUserByCar(Car car) {
+      final String HQL = "FROM User user JOIN FETCH user.car WHERE user.car.model=:Model AND user.car.series=:Series";
+      return sessionFactory.getCurrentSession()
+              .createQuery(HQL, User.class)
+              .setParameter("Model", car.getModel())
+              .setParameter("Series", car.getSeries())
+              .uniqueResult();
    }
 
 }
